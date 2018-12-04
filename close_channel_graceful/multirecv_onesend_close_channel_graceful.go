f8289668afdb8dbe4dfa5a3418a28690e3e989f7
haolipeng <1078285863@qq.com>
2018-12-04 14:03:20 +0800

    1.一个生产者，多个消费者 2.多个生产者，多个消费者 3.多个生产者，一个消费者 三种情况下的优雅关闭channel通道

---
+++ close_channel_graceful/multirecv_onesend_close_channel_graceful.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"log"
+	"sync"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	log.SetFlags(0)
+
+	const MaxRandomNumber = 10000
+	const NumReceivers = 100
+
+	wgReceives := sync.WaitGroup{}
+	wgReceives.Add(NumReceivers)
+
+	dataCh := make(chan int, 100)
+
+	//the sender
+	go func() {
+		for {
+			if value := rand.Intn(MaxRandomNumber); value == 0 {
+				close(dataCh)
+				return
+			} else {
+				dataCh <- value
+			}
+		}
+	}()
+
+	//receivers
+	for i := 0; i < NumReceivers; i++ {
+		go func() {
+			defer wgReceives.Done()
+
+			//receive values until dataCh is closed and the value buffer queue of dataCh is empty
+			for value := range dataCh {
+				log.Println(value)
+			}
+		}()
+	}
+	wgReceives.Wait()
+}
---
+++ close_channel_graceful/onerecv_multisend_close_channel_graceful.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"log"
+	"sync"
+)
+
+//the receiver says "please stop sending more" by closing an additional signal channel
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	log.SetFlags(0)
+
+	const MaxRandomNumber = 10000
+	const NumSenders = 2
+
+	wgReceives := sync.WaitGroup{}
+	wgReceives.Add(1)
+
+	dataCh := make(chan int, 100)
+	stopCh := make(chan struct{})
+	// stopCh is an additional signal channel.
+	// Its sender is the receiver of channel dataCh.
+	// Its reveivers are the senders of channel dataCh.
+
+	//senders,one sender create a goroutine
+	for i := 0; i < NumSenders; i++ {
+		go func() {
+			for {
+				select {
+				case <-stopCh:
+					return
+				default: //comment this line may cause panic
+				}
+
+				select {
+				case <-stopCh:
+					return
+				case dataCh <- rand.Intn(MaxRandomNumber):
+				}
+			}
+		}()
+	}
+
+	//one receiver
+	go func() {
+		defer wgReceives.Done()
+		for value := range dataCh {
+			if value == MaxRandomNumber-1 {
+				//close stopChan additional channel to modify senders
+				close(stopCh)
+				return
+			}
+
+			log.Println(value)
+		}
+	}()
+	wgReceives.Wait()
+}
