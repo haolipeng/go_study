c86e7d747c69dce8871ce3097bc5c9a57d9a7877
haolipeng <1078285863@qq.com>
2019-04-20 00:25:28 +0800

    文件下载接口

--- filestore_server/handler/handler.go
+++ filestore_server/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"go_study/filestore_server/meta"
 	"go_study/filestore_server/util"
@@ -11,8 +12,7 @@ import (
 	"time"
 )
 
-const FILE_PATH = "/tmp"
-
+//UploadFile:文件上传
 func UploadFile(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		//返回上传index.html页面
@@ -53,13 +53,17 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 		//拷贝数据
 		fMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
-			fmt.Printf("written size %d is not equal to file size %d", writtenSize, fileHeader.Size)
+			fmt.Printf("written size %d is not equal to file size %d", fMeta.FileSize, fileHeader.Size)
 
 		}
 
 		//计算sha1值
 		newFile.Seek(0, 0)
 		fMeta.FileSha1 = util.FileSha1(newFile)
+		fmt.Printf("file sha1 is %s\n", fMeta.FileSha1)
+
+		//添加到文件元集合中
+		meta.UpdateFileMeta(fMeta)
 
 		//重定向
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
@@ -69,10 +73,58 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//UploadFileSuccess:文件上传成功
 func UploadFileSuccess(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" || r.Method == "POST" {
 		io.WriteString(w, "upload success!")
 	} else {
 		io.WriteString(w, "not supported file operation")
 	}
+}
+
+//FileQuery:文件元信息查询
+func FileQuery(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fileSha1 := r.Form["filehash"][0]
+
+	fMeta := meta.GetFileMeta(fileSha1)
+
+	//fix me GetFileMeta may return nil
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		//向http头部写入状态码
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	//返回json后的数据
+	w.Write(data)
+}
+
+func DownloadFile(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fileSha1 := r.Form.Get("filehash")
+	fMeta := meta.GetFileMeta(fileSha1)
+
+	//打开文件
+	f, err := os.Open(fMeta.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	//fix me if file is too big ,ReadAll is not good
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	//添加http头，让浏览器能识别出是文件下载
+	w.Header().Set("Content-Type", "application/octect-stream")
+	w.Header().Set("content-disposition", "attachment;filename=\""+fMeta.FileName+"\"")
+
+	w.Write(data)
 }
--- filestore_server/main.go
+++ filestore_server/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
+	//路由规则
 	http.HandleFunc("/file/upload/suc", handler.UploadFileSuccess)
 	http.HandleFunc("/file/upload", handler.UploadFile)
+	http.HandleFunc("/file/meta", handler.FileQuery)
+	http.HandleFunc("/file/download", handler.DownloadFile)
 	http.ListenAndServe(":9000", nil)
 }
