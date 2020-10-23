efab9d87d339167ea10ba9e6c05680f6a8ec3e53
haolipeng <1078285863@qq.com>
2020-10-24 07:45:11 +0800

    完善map的例子

--- grammar_exercise/map_exercise/map.go
+++ grammar_exercise/map_exercise/map.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	//map create
+	//空map无法使用，需要显式初始化或make
+	//panic: assignment to entry in nil map
+	//var nilMap  map[string] string
+	//nilMap["name"] = "haolipeng"
+
+	// 1.map 创建及初始化
 	m := map[string]string{
 		"name":    "ccmouse",
 		"course":  "golang",
@@ -13,12 +18,15 @@ func main() {
 
 	//m2 := make(map[string] string)
 
-	//map 遍历 hashmap 不是有序map
+	// 2.遍历操作
+	// hashmap 不是有序map
+	fmt.Println("-----------遍历--------------")
 	for k, v := range m {
 		fmt.Println(k, v)
 	}
 
 	//map获取元素,可用第二个值ok判断key在map中是否存在
+	fmt.Println("-----------判断值是否存在--------------")
 	fmt.Println("Getting Vaules")
 	courseName, ok := m["course"]
 	fmt.Println(courseName, ok)
@@ -31,15 +39,27 @@ func main() {
 	}
 
 	//删除元素
+	fmt.Println("-----------删除元素--------------")
 	fmt.Println("Deleting Values")
 	name, ok := m["name"]
 	fmt.Println(name, ok)
 
 	delete(m, "name")
 
-	name, ok = m["name"] //declare before
-	fmt.Println(name, ok)
+	name, ok = m["name"]
+	if !ok {
+		fmt.Printf("%s element is removed\n", "name")
+	}
 
-	//map的key和value的类型
+	//map作为函数参数
+	modifyMap(m)
+	fmt.Println("-----------调用函数后遍历--------------")
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+}
 
+//map是引用类型,以函数参数形式存在，函数内部修改会影响指向的同一个map
+func modifyMap(m map[string]string) {
+	m["course"] = "C++"
 }
