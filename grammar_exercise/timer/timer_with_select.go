12662047c2afc9b4b71478d4982527428384438e
haolipeng <1078285863@qq.com>
2020-10-24 16:52:52 +0800

    golang 定时器 timer，ticker，time.After()注意事项

--- grammar_exercise/timer/timer_with_select.go
+++ grammar_exercise/timer/timer_with_select.go
@@ -9,8 +9,11 @@ import (
 //Ticker和Timer的区别是什么?
 //Ticker是永久定时器，如过不调用Stop函数就会一直触发
 //Timer是单次定时器，如果想定时器再次触发，则需调用Reset函数
-
-//数据生产者 从channel通道中读取数据20次
+//深入理解下time.After
+/*
+* 模拟生产者和消费者模型，对数据进行处理
+ */
+//数据生产者 写入数据到channel中20次
 func dataProducer(ch chan int) {
 	for i := 0; i < 20; i++ {
 		ch <- i
@@ -18,7 +21,7 @@ func dataProducer(ch chan int) {
 	}
 }
 
-//数据消费者,使用多路选择器从channel通道中读取数据，和定时器一起使用
+//数据消费者,使用select从channel通道中读取数据，和定时器一起使用
 func dataConsumer(ch chan int) {
 	tm := time.NewTimer(time.Second)
 	for {
@@ -29,7 +32,11 @@ func dataConsumer(ch chan int) {
 			fmt.Println("call new timer!")
 			tm.Reset(time.Second)
 		//如果ch通道中持续有数据，则不会触发此case语句
+		/********************重要的话说三遍******************/
 		//每进行一次select选择时，time.After会重新创建一个定时器，而以前的定时器还遗留在时间堆中等待gc释放
+		///进行一次select选择时，time.After会重新创建一个定时器，而以前的定时器还遗留在时间堆中等待gc释放
+		//每进行一次select选择时，time.After会重新创建一个定时器，而以前的定时器还遗留在时间堆中等待gc释放
+		//如果定时器没有到达定时时间，则gc不会启动垃圾回收
 		case <-time.After(time.Second):
 			fmt.Println("timeout")
 		}
@@ -41,17 +48,22 @@ func timeAfterInSelect() {
 	ch := make(chan int, 1)
 	go dataProducer(ch)
 	go dataConsumer(ch)
+
+	//等待10秒，输出结果
+	time.Sleep(10 * time.Second)
 }
 
 /*Tick是对NewTicker的封装*/
 func useTick() {
-	c := time.Tick(time.Second)
+	c := time.Tick(time.Second) //每次走一秒，输出日期
 	for now := range c {
 		fmt.Println("time at ", now)
 	}
 }
 
-/*NewTimer 例子*/
+/*NewTimer 例子
+Timer定时器是一次性定时器，想执行下次就需要手动Reset
+*/
 func useNewTimer() {
 	onceTimer := time.NewTimer(time.Second)
 	everyTimer := time.NewTimer(time.Second * 2)
@@ -84,7 +96,7 @@ func useNewTicker() {
 		case <-done:
 			fmt.Println("get Done!")
 			return
-		case t := <-ticker.C:
+		case t := <-ticker.C: //输出当前时间
 			fmt.Println("Current time:", t)
 		}
 	}
@@ -108,40 +120,15 @@ func useNewTimerWithContext(ctx context.Context) {
 	}
 }
 
-/*func main() {
+func main() {
 	//useTick()
 	//useNewTimer()
 	//useNewTicker()
-	//timeAfterInSelect()
-	ctx, _ := context.WithTimeout(context.Background(), time.Duration(8 * time.Second))
-	go useNewTimerWithContext(ctx)
 
-	time.Sleep(30 * time.Second)
-}
-*/
+	//在select中使用time.After
+	timeAfterInSelect()
+	//ctx, _ := context.WithTimeout(context.Background(), time.Duration(8 * time.Second))
+	//go useNewTimerWithContext(ctx)
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	go watch(ctx, "【监控1】")
-	go watch(ctx, "【监控2】")
-	go watch(ctx, "【监控3】")
-
-	time.Sleep(10 * time.Second)
-	fmt.Println("可以了，通知监控停止")
-	cancel()
-	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
-	time.Sleep(5 * time.Second)
-}
-
-func watch(ctx context.Context, name string) {
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println(name, "监控退出，停止了...")
-			return
-		default:
-			fmt.Println(name, "goroutine监控中...")
-			time.Sleep(2 * time.Second)
-		}
-	}
+	//time.Sleep(30 * time.Second)
 }
