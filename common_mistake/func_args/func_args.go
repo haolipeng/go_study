65598170963a2fc6d6d17f7804e7213704f20dd9
haolipeng <lipeng.hao@colasoft.com.cn>
2020-12-16 21:53:11 +0800

    slice or array as function arguments

--- common_mistake/func_args/func_args.go
+++ common_mistake/func_args/func_args.go
@@ -2,26 +2,31 @@ package main
 
 import "fmt"
 
-func array_func_args(arr [3]int) {
+//数组作为函数参数,golang和C/C++不同，golang向函数传递数组时，函数会得到原始数组数据的一份拷贝
+func FuncArgsIsArray(arr [3]int) {
+	fmt.Printf("in function array pointer:%p\n", &arr)
 	arr[0] = 10
-	fmt.Println("after array modify, array:", arr)
+	fmt.Println("in function modify, array:", arr)
 }
 
-func slice_func_args(arr []int) {
-	arr[0] = 100
-	fmt.Println("after slice modify, array:", arr)
+//切片作为函数参数
+func FuncArgsIsSlice(s []int) {
+	fmt.Printf("in function slice pointer:%p\n", s)
+	s[0] = 10
+	fmt.Println("in function modify, slice:", s)
 }
 
 //向函数中传递数组时，函数会得到原始数组数据的一份拷贝，如果在函数内部想更新数据，是不可能的
 func main() {
-	//数组
+	fmt.Println("--------------测试数组作为函数参数--------------")
 	x := [3]int{1, 2, 3}
-	array_func_args(x)
-	fmt.Println(x)
+	fmt.Printf("before function array pointer:%p\n", &x)
+	FuncArgsIsArray(x)
+	fmt.Println("after array modify, array:", x)
 
-	fmt.Println("----------------------------")
-	//slice
+	fmt.Println("--------------测试切片作为函数参数--------------")
 	y := []int{1, 2, 3}
-	slice_func_args(y)
-	fmt.Println(y)
+	fmt.Printf("before function slice pointer:%p\n", y)
+	FuncArgsIsSlice(y)
+	fmt.Println("after slice modify, slice:", y)
 }
