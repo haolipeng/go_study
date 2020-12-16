8d5e3fc71e766011d7e2b09a610f227fffe5e465
haolipeng <lipeng.hao@colasoft.com.cn>
2020-12-16 22:16:06 +0800

    以小写字母开头的结构体将不会被（json、xml、gob等）编码，因此当你编码这些未导出的结构体时，你将会得到零值。

---
+++ common_mistake/json/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type MyData struct {
+	One int
+	two string
+}
+
+func main() {
+	in := MyData{1, "two"}
+	fmt.Printf("%#v\n", in) //prints main.MyData{One:1, two:"two"}
+
+	//json序列化
+	encoded, _ := json.Marshal(in)
+	fmt.Println(string(encoded)) //prints {"One":1}
+
+	//json反序列化
+	var out MyData
+	json.Unmarshal(encoded, &out)
+	fmt.Printf("%#v\n", out) //prints main.MyData{One:1, two:""}
+}
