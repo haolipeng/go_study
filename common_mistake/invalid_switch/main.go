427304b5f8f4e3838ed586d8ece98dcf4f02f27d
haolipeng <1078285863@qq.com>
2020-12-16 22:10:01 +0800

    invalid switch case and fallthrough

--- common_mistake/invalid_switch/main.go
+++ common_mistake/invalid_switch/main.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+//你可以通过在每个“case”块的结尾使用“fallthrough”，来强制“case”代码块进入。
+//你也可以重写switch语句，来使用“case”块中的表达式列表。
 func main() {
 	isSpace := func(ch byte) bool {
 		switch ch {
-		case ' ':
+		case ' ': //这个case不会return true，除非使用fallthrough
 		case '\t':
 			return true
 		}
@@ -13,4 +15,15 @@ func main() {
 	}
 	fmt.Println(isSpace('\t')) //true
 	fmt.Println(isSpace(' '))  //false
+
+	//正确写法
+	isSpaceGood := func(ch byte) bool {
+		switch ch {
+		case ' ', '\t':
+			return true
+		}
+		return false
+	}
+	fmt.Println(isSpaceGood('\t')) //true
+	fmt.Println(isSpaceGood(' '))  //true
 }
