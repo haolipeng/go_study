86603d0f423bce801c5be9038735119b0d43cc39
haolipeng <lipeng.hao@colasoft.com.cn>
2020-12-16 21:37:29 +0800

    nil string is a empty string

--- common_mistake/string/nil_string.go
+++ common_mistake/string/nil_string.go
@@ -7,5 +7,11 @@ func main() {
 	//nil is a predeclared identifier representing the zero value for
 	//a pointer, channel, func, interface, map, or slice type.
 	//Type must be a pointer, channel, func, interface, map, or slice type
+
+	var x string //默认值为空字符串""
+	if x == "" {
+		fmt.Println("x string is default empty string")
+	}
+
 	fmt.Println("string can't be nil")
 }
