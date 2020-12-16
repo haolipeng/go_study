261196c09bcade8901c33f657bc144fb6162d753
haolipeng <lipeng.hao@colasoft.com.cn>
2020-12-16 23:50:13 +0800

    提供高级版的for-range技巧demo

--- common_mistake/for-range/range_slice_array.go
+++ common_mistake/for-range/range_slice_array.go
@@ -2,47 +2,65 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
+type field struct {
+	name string
+}
+
+func (p *field) print() {
+	fmt.Println(p.name)
+}
+
 func main() {
-	//test 1
-	rangeSlice()
+	//test 0
+	fmt.Println("-----------------test1----------------------")
+	testSlice()
+	time.Sleep(3 * time.Second)
 
-	//test 2
-	x := []string{"a", "b", "c"}
+	fmt.Println("-----------------test2----------------------")
+	data1 := []*field{{"one"}, {"two"}, {"three"}}
+	for _, v := range data1 {
+		go v.print()
+	}
+	time.Sleep(3 * time.Second)
+	//goroutines print: one, two, three
 
-	//遍历slice时，返回的第一个值是index索引
-	for _, v := range x {
-		fmt.Printf("out of goroutine -> value:%v pointer:%v\n", v, &v)
+	fmt.Println("-----------------test3----------------------")
+	data2 := []field{{"one"}, {"two"}, {"three"}}
+	for _, v := range data2 {
+		go v.print()
 	}
-	fmt.Println("-------------------------------------------------")
-	for _, v := range x {
-		go func() {
-			fmt.Printf("in goroutine -> value:%v pointer:%v\n", v, &v)
-		}()
-	}
+	//goroutines print: three, three, three
+
+	/*
+		test 1
+		x := []string{"a", "b", "c"}
+
+		//遍历slice时，返回的第一个值是index索引
+		for _, v := range x {
+			fmt.Printf("out of goroutine -> value:%v pointer:%v\n", v, &v)
+		}
+		fmt.Println("-------------------------------------------------")
+		for _, v := range x {
+			go func() {
+				fmt.Printf("in goroutine -> value:%v pointer:%v\n", v, &v)
+			}()
+			//time.Sleep(time.Second)
+		}*/
 
 	time.Sleep(4 * time.Second)
 }
 
-func rangeSlice() {
-	//Using goroutines on loop iterator variables
-	var values = []string{
-		"a",
-		"b",
-		"c",
-	}
-	var wg sync.WaitGroup
-	for _, val := range values {
-		wg.Add(1)
+//1.选择将变量作为函数的参数传递
+//2.将迭代的变量值保存到一个临时变量中
+func testSlice() {
+	data := []string{"one", "two", "three"}
+	for _, v := range data {
 		go func() {
-			defer wg.Done()
-			fmt.Println(val)
+			fmt.Println(v)
 		}()
 	}
-
-	wg.Wait()
-	fmt.Println("the program has exited")
+	//goroutines print: three, three, three
 }
