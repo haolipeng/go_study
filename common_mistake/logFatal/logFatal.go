63e55cada22c2e16984684e34144f4cd83dc8d07
haolipeng <lipeng.hao@colasoft.com.cn>
2020-12-16 17:55:56 +0800

    测试单例模式

--- common_mistake/logFatal/logFatal.go
+++ common_mistake/logFatal/logFatal.go
@@ -2,6 +2,13 @@ package main
 
 import "log"
 
+/*
+func Fatal(v ...interface{}) {
+	std.Output(2, fmt.Sprint(v...))
+	os.Exit(1)
+}
+*/
+//log.Fatal函数中已经包含os.Exit(1)
 func main() {
 	log.Fatalln("Fatal Level: log entry") //app exits here
 	log.Println("Normal Level: log entry")
