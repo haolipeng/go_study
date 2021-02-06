a77ff6720b7997c3a9b87c1528292c41ccfdf767
haolipeng <1078285863@qq.com>
2021-02-06 23:04:03 +0800

    list容器的增删改查练习代码

---
+++ grammar_exercise/container/list/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+//遍历list各个节点
+func ShowList(l *list.List) {
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+}
+
+func main() {
+	// 创建list
+	l := list.New()
+
+	//清空list
+	l.Init()
+
+	// 插入元素
+	e4 := l.PushBack(4)
+	e1 := l.PushFront(1)
+	e2 := l.InsertAfter(2, e1)
+	e3 := l.InsertBefore(3, e4)
+
+	ShowList(l)
+
+	//首尾元素
+	fmt.Printf("Len: %d Front Element:%d Front Element:%d\n", l.Len(), l.Front().Value, l.Back().Value)
+
+	//演示MoveAfter,MoveBefore,MoveToBack,MoveToFront
+	fmt.Println("==================after call MoveAfter==================")
+	l.MoveAfter(e2, e3)
+	ShowList(l)
+
+	fmt.Println("==================after call MoveBefore==================")
+	l.MoveBefore(e2, e3)
+	ShowList(l)
+
+	fmt.Println("==================after call MoveToBack==================")
+	l.MoveToBack(e1)
+	ShowList(l)
+
+	fmt.Println("==================after call MoveToFront==================")
+	l.MoveToFront(e1)
+	ShowList(l)
+}
