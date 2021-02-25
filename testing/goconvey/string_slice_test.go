ba6b96dccfcff7465fea894c7e8576510864c64a
haolipeng <lipeng.hao@colasoft.com.cn>
2021-02-25 13:59:10 +0800

    goconvey测试框架例子

--- testing/goconvey/string_slice_test.go
+++ testing/goconvey/string_slice_test.go
@@ -5,19 +5,57 @@ import (
 )
 import . "github.com/smartystreets/goconvey/convey"
 
-//Convey用于定义测试
+//Convey用于定义测试，Convey可以嵌套使用
 //So用来做判断
-//Convey可以嵌套使用
 func TestStringSliceEqual(t *testing.T) {
 	Convey("TestStringSliceEqual的描述", t, func() {
+		//一次测试了多种情况
 		Convey("true when a!=nil && b != nil", func() {
 			a := []string{"hello", "goconvey"}
 			b := []string{"hello", "goconvey"}
 			So(StringSliceEqual(a, b), ShouldBeTrue)
 		})
 
-		Convey("true when a == nil && b == nil", func() {
+		Convey("TestStringSliceEqual should return true when a ＝= nil  && b ＝= nil", func() {
 			So(StringSliceEqual(nil, nil), ShouldBeTrue)
+		})
+
+		Convey("TestStringSliceEqual should return false when a ＝= nil  && b != nil", func() {
+			a := []string(nil)
+			b := []string{}
+			So(StringSliceEqual(a, b), ShouldBeFalse)
+		})
+
+		Convey("TestStringSliceEqual should return false when a != nil  && b != nil", func() {
+			a := []string{"hello", "world"}
+			b := []string{"hello", "goconvey"}
+			So(StringSliceEqual(a, b), ShouldBeFalse)
+		})
+	})
+}
+
+func TestStringSliceEqualWithNested(t *testing.T) {
+	Convey("TestStringSliceEqual", t, func() {
+		Convey("should return true when a != nil  && b != nil", func() {
+			a := []string{"hello", "goconvey"}
+			b := []string{"hello", "goconvey"}
+			So(StringSliceEqual(a, b), ShouldBeTrue)
+		})
+
+		Convey("should return true when a ＝= nil  && b ＝= nil", func() {
+			So(StringSliceEqual(nil, nil), ShouldBeTrue)
+		})
+
+		Convey("should return false when a ＝= nil  && b != nil", func() {
+			a := []string(nil)
+			b := []string{}
+			So(StringSliceEqual(a, b), ShouldBeFalse)
+		})
+
+		Convey("should return false when a != nil  && b != nil", func() {
+			a := []string{"hello", "world"}
+			b := []string{"hello", "goconvey"}
+			So(StringSliceEqual(a, b), ShouldBeFalse)
 		})
 	})
 }
