952622b900ec625a65dba4bce5197bee3c5e4217
haolipeng <lipeng.hao@colasoft.com.cn>
2021-03-01 14:41:55 +0800

    1、golang helper帮助函数

--- testing/standard/helper_test.go
+++ testing/standard/helper_test.go
@@ -7,6 +7,8 @@ type calcCases struct {
 }
 
 func testMulHelper(t *testing.T, cases *calcCases) {
+	//有些帮助函数还可能在不同的函数中被调用，报错信息都在同一处，不方便问题定位。
+	t.Helper() //不加此行会导致错误定位不准
 	if val := Mul(cases.A, cases.B); val != cases.Expected {
 		t.Fatalf("%d * %d expected %d, but %d got",
 			cases.A, cases.B, cases.Expected, val)
@@ -26,7 +28,7 @@ func TestMulWithHelp(t *testing.T) {
 		Expected: -6,
 	})
 
-	testMulHelper(t, &calcCases{
+	testMulHelper(t, &calcCases{ //加上t.Helper()后定位到这里
 		A:        2,
 		B:        0,
 		Expected: 1,
