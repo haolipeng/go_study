b25bbc6d4210e637b38a0fb8e6d25aff3ee758bd
haolipeng <lipeng.hao@colasoft.com.cn>
2021-03-08 14:23:48 +0800

    修改goconvey库的例子

--- testing/goconvey/string_slice_test.go
+++ testing/goconvey/string_slice_test.go
@@ -5,35 +5,17 @@ import (
 )
 import . "github.com/smartystreets/goconvey/convey"
 
-//Convey用于定义测试，Convey可以嵌套使用
-//So用来做判断
+//GoConvey用于定义测试，Convey可以嵌套使用
+//goconvey一般用So来做断言，断言时可以传入一个函数
 func TestStringSliceEqual(t *testing.T) {
 	Convey("TestStringSliceEqual的描述", t, func() {
-		//一次测试了多种情况
-		Convey("true when a!=nil && b != nil", func() {
-			a := []string{"hello", "goconvey"}
-			b := []string{"hello", "goconvey"}
-			So(StringSliceEqual(a, b), ShouldBeTrue)
-		})
-
-		Convey("TestStringSliceEqual should return true when a ＝= nil  && b ＝= nil", func() {
-			So(StringSliceEqual(nil, nil), ShouldBeTrue)
-		})
-
-		Convey("TestStringSliceEqual should return false when a ＝= nil  && b != nil", func() {
-			a := []string(nil)
-			b := []string{}
-			So(StringSliceEqual(a, b), ShouldBeFalse)
-		})
-
-		Convey("TestStringSliceEqual should return false when a != nil  && b != nil", func() {
-			a := []string{"hello", "world"}
-			b := []string{"hello", "goconvey"}
-			So(StringSliceEqual(a, b), ShouldBeFalse)
-		})
+		a := []string{"hello", "goconvey"}
+		b := []string{"hello", "goconvey"}
+		So(StringSliceEqual(a, b), ShouldBeTrue)
 	})
 }
 
+//双层嵌套
 func TestStringSliceEqualWithNested(t *testing.T) {
 	Convey("TestStringSliceEqual", t, func() {
 		Convey("should return true when a != nil  && b != nil", func() {
