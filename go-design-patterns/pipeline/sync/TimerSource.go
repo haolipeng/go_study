bf7c3aa8aa6a05a79f08c5a065efb5d2ef460b46
haolipeng <1078285863@qq.com>
2025-02-18 10:44:31 +0800

    fix bug

--- go-design-patterns/pipeline/sync/TimerSource.go
+++ go-design-patterns/pipeline/sync/TimerSource.go
@@ -28,9 +28,11 @@ func (s *TimerSource) Process(ctx context.Context) (<-chan any, error) {
 
 	// 创建定时器，避免重复创建
 	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop() // 确保资源释放
 
 	go func() {
+		// 确保资源timer释放
+		defer ticker.Stop()
+
 		//协程退出时，释放channel资源
 		defer close(out)
 
@@ -39,6 +41,7 @@ func (s *TimerSource) Process(ctx context.Context) (<-chan any, error) {
 			case <-ticker.C: // 使用 ticker 替代 time.After
 				select {
 				case out <- string(input): // 数据发送成功
+					log.Println("timer success data input!")
 				default:
 					// channel已满，记录日志
 					log.Println("Channel full, dropping message")
