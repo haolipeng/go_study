b115512669a0c739f39b246cf2ff89af66c669fd
haolipeng <1078285863@qq.com>
2025-02-18 10:54:56 +0800

    add test:multiple concurrency

--- go-design-patterns/pipeline/sync/pipeline_test.go
+++ go-design-patterns/pipeline/sync/pipeline_test.go
@@ -32,3 +32,30 @@ func TestProcessorManager_SingleConcurrency(t *testing.T) {
 		return
 	}
 }
+
+// 测试多个并发度
+func TestProcessorManager_MultiConcurrency(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	m := NewProcessorManager()
+
+	//pipeline 组装
+	m.AddSource(NewTimerSource(LINES))
+	m.AddSink(NewConsoleSink())
+
+	m.AddProcessor(&SplitProcessor{})
+	m.AddProcessor(&CountProcessor{})
+	m.AddProcessor(&SortProcessor{})
+
+	//定时通知退出
+	go func() {
+		time.Sleep(3 * time.Second)
+		cancel()
+	}()
+
+	const routineCnt = 5
+	err := m.RunN(ctx, routineCnt)
+	if err != nil {
+		log.Printf("Run error:%s\n", err)
+		return
+	}
+}
